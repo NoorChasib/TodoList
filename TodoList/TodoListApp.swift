57edce5339c7ef16a0ccc482cbbacdb8da6c0f0f
Noor Chasib <noorchasib@gmail.com>
2023-04-08 15:09:06 -0700

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Noor Chasib on 2023-04-08.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
